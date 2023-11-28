ffb1b028fc64451b2056ba9838f578fab211b24f
Nick Strayer <nick.strayer@rstudio.com>
2023-11-28 17:23:14 -0500

    Update output binding helper to be easier to use with complex payload types

--- packages/main/src/index.ts
+++ packages/main/src/index.ts
@@ -1,7 +1,7 @@
 export type { CustomElementInput } from "./makeInputBinding";
 export { makeInputBinding } from "./makeInputBinding";
 export { makeOutputBinding } from "./makeOutputBinding";
-
+export type { CustomElementOutput } from "./makeOutputBinding";
 // type Shiny = typeof window.Shiny | undefined;
 // export type { Shiny };
 
--- packages/main/src/makeOutputBinding.ts
+++ packages/main/src/makeOutputBinding.ts
@@ -3,19 +3,32 @@ import { Shiny } from "./OptionalShiny";
 /**
  * A custom element that extends this interface will be treated as an output
  * binding by Shiny when paired with `makeOutputBinding()`.
+ * @template T The type of the data that will be passed to the element
+ * @property onNewValue A function that will be called when there is new data
+ * to render
  */
 export interface CustomElementOutput<T> extends HTMLElement {
-  value: T;
+  onNewValue: (x: T) => void;
 }
 
 /**
  * Given a tag name for a custom element that extends CustomElementOutput<T>,
  * this will hook up the proper output binding and register it with Shiny.
  * @param tagName Name of the tag that corresponds to the output binding
- * @returns Nothing
+ * @param el The custom element that extends CustomElementOutput<T>
+ * @param opts Options for the output binding
+ * @param opts.registerElement Whether to register the webcomponent used for the
+ * output. Defaults to true.
  */
-export function makeOutputBinding<El extends CustomElementOutput<T>, T>(
-  tagName: string
+export function makeOutputBinding<
+  T,
+  El extends CustomElementOutput<T> = CustomElementOutput<T>
+>(
+  tagName: string,
+  el: Constructor<El>,
+  opts: {
+    registerElement?: boolean;
+  } = { registerElement: true }
 ) {
   if (!Shiny) {
     return;
@@ -27,9 +40,18 @@ export function makeOutputBinding<El extends CustomElementOutput<T>, T>(
     }
 
     override renderValue(el: El, data: T): void {
-      el.value = data;
+      el.onNewValue(data);
     }
+  }
+
+  if (opts.registerElement) {
+    customElements.define(tagName, el);
   }
 
   Shiny.outputBindings.register(new NewCustomBinding(), `${tagName}-Binding`);
 }
+
+type Constructor<T> = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  new (...args: any[]): T;
+};
