fabc2263b101b2c75d37adfc8d8878d97e62fc39
Nick Strayer <nick.strayer@rstudio.com>
2023-12-07 11:06:47 -0500

    Fix dependency structure to reflect new npm names

--- packages/react/src/makeReactInput.ts
+++ packages/react/src/makeReactInput.ts
@@ -1,7 +1,7 @@
 import {
   CustomElementInput,
   makeInputBinding,
-} from "@posit-dev/shiny-helpers-main";
+} from "@posit-dev/shiny-binding-main";
 import type { ReactNode } from "react";
 import { createRoot } from "react-dom/client";
 
